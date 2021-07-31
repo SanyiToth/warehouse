c2f0b222fadc421bfbe99d6cca1e44abdfea0884
SanyiToth <sandor.toth@hotmail.com>
2021-07-31 17:46:07 +0200

    add new button created

--- src/app/shared/table-list/table-list.module.ts
+++ src/app/shared/table-list/table-list.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableListComponent } from './table-list.component';
 import {MatTableModule} from "@angular/material/table";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 
 
@@ -14,7 +16,10 @@ import {MatTableModule} from "@angular/material/table";
     ],
   imports: [
     CommonModule,
-    MatTableModule
+    MatIconModule,
+    MatTableModule,
+    MatButtonModule,
+
   ]
 })
 export class TableListModule { }
