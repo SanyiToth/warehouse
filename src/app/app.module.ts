2da4f07e598f465542788578726a07a620cbab7f
SanyiToth <sandor.toth@hotmail.com>
2021-08-02 14:00:02 +0200

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {CoreModule} from "./core/core.module";
-import {MatMenuModule} from "@angular/material/menu";
-import {MatIconModule} from "@angular/material/icon";
-import {MatButtonModule} from "@angular/material/button";
-
 
 @NgModule({
   declarations: [
@@ -14,9 +10,6 @@ import {MatButtonModule} from "@angular/material/button";
   imports: [
     CoreModule,
     BrowserModule,
-    MatMenuModule,
-    MatIconModule,
-    MatButtonModule
   ],
   providers: [],
   exports: [
