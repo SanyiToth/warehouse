24630794479cb914b919ca720323ab5ec7381c53
SanyiToth <sandor.toth@hotmail.com>
2021-08-06 22:36:44 +0200

    refactor

--- src/app/feature/products/products-list/products-list.component.ts
+++ src/app/feature/products/products-list/products-list.component.ts
@@ -4,7 +4,6 @@ import {MatDialog, MatDialogConfig, MatDialogRef} from "@angular/material/dialog
 import {ConfirmDialogComponent} from "../../../shared/confirm-dialog/confirm-dialog.component";
 import {ProductDialogComponent} from "../product-dialog/product-dialog.component";
 import {AuthService} from "../../../shared/auth/auth.service";
-import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-products-list',
@@ -19,6 +18,13 @@ export class ProductsListComponent implements OnInit {
   displayedColumns!: string[];
   isLoggedIn!: boolean;
 
+  dialogRefConfirm!: MatDialogRef<ConfirmDialogComponent> | null;
+  dialogRefProduct!: MatDialogRef<ProductDialogComponent> | null;
+
+  constructor(private auth: AuthService,
+              private dialog: MatDialog) {
+  }
+
   ngOnInit(): void {
 
     this.displayedColumns = ["name", "width", "length", "date", "actions"];
@@ -30,33 +36,17 @@ export class ProductsListComponent implements OnInit {
     this.dataToParent.emit($event);
   }
 
-  dialogRefConfirm!: MatDialogRef<ConfirmDialogComponent> | null;
-  dialogRefProduct!: MatDialogRef<ProductDialogComponent> | null;
 
-  @Input() element!: any;
-  @Output() deletedElement = new EventEmitter<Product>();
-
-
-  activatedRoutePath: string | undefined = this.route.routeConfig?.path;
-
-
-  constructor(private auth: AuthService,
-              private dialog: MatDialog,
-              private route: ActivatedRoute) {
-  }
-
-
-  onDelete() {
+  onDelete(element: Product) {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = false;
     dialogConfig.data = {
-      element: this.element
+      element: element
     };
     this.dialogRefConfirm = this.dialog.open(ConfirmDialogComponent, dialogConfig);
     this.dialogRefConfirm.afterClosed()
       .subscribe(resp => {
-        console.log('resp', resp)
-        if (resp) this.deletedElement.emit(this.element);
+        if (resp) this.dataToParent.emit(element);
         this.dialogRefConfirm = null;
       });
   }
