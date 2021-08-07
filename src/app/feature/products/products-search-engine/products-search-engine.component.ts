bdf1bfb55ac7807c89864d3ee25c670df71b992b
SanyiToth <sandor.toth@hotmail.com>
2021-08-07 10:44:07 +0200

    search engine component generated

---
+++ src/app/feature/products/products-search-engine/products-search-engine.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-search-engine',
+  templateUrl: './products-search-engine.component.html',
+  styleUrls: ['./products-search-engine.component.css']
+})
+export class ProductsSearchEngineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
