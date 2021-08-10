6eec2eb527977a93bb6f5fe75ecf7f85ac1d5404
SanyiToth <sandor.toth@hotmail.com>
2021-08-10 10:19:04 +0200

    header module imported in stores module

--- src/app/feature/stores/stores.module.ts
+++ src/app/feature/stores/stores.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoresContainerComponent } from './stores-container/stores-container.component';
+import {HeaderModule} from "../../shared/header/header.module";
 
 
 
@@ -9,7 +10,8 @@ import { StoresContainerComponent } from './stores-container/stores-container.co
     StoresContainerComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HeaderModule
   ]
 })
 export class StoresModule { }
