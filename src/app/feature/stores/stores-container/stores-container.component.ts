32e2cd6e26201f604c699bd9f7ec4c2d7e467955
SanyiToth <sandor.toth@hotmail.com>
2021-08-10 12:18:43 +0200

    storeId added

--- src/app/feature/stores/stores-container/stores-container.component.ts
+++ src/app/feature/stores/stores-container/stores-container.component.ts
@@ -43,9 +43,22 @@ export class StoresContainerComponent implements OnInit {
       .afterClosed()
       .subscribe(storeDialogValues => {
         if (storeDialogValues) {
+          storeDialogValues.storeId = StoresContainerComponent.generateStoreId(6);
           this.onAdd(storeDialogValues);
         }
       });
+  }
+
+
+  private static generateStoreId(length: number) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() *
+        charactersLength));
+    }
+    return result;
   }
 
 
