32d0d7d0b9e0818cd367f10593abddc9f692037b
SanyiToth <sandor.toth@hotmail.com>
2021-08-10 10:26:28 +0200

    store list created

--- src/app/feature/stores/stores.module.ts
+++ src/app/feature/stores/stores.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoresContainerComponent } from './stores-container/stores-container.component';
 import {HeaderModule} from "../../shared/header/header.module";
+import { StoresListComponent } from './stores-list/stores-list.component';
 
 
 
 @NgModule({
   declarations: [
-    StoresContainerComponent
+    StoresContainerComponent,
+    StoresListComponent
   ],
   imports: [
     CommonModule,
