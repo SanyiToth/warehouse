af771b62c5c821676be5565daa2f75cdae32a89d
SanyiToth <sandor.toth@hotmail.com>
2021-08-10 10:06:52 +0200

    store module and container component generated

---
+++ src/app/feature/stores/stores.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StoresContainerComponent } from './stores-container/stores-container.component';
+
+
+
+@NgModule({
+  declarations: [
+    StoresContainerComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class StoresModule { }
