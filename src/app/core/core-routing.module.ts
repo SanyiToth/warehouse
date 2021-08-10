9a1156130f1c760d3c54359305a68496928adef3
SanyiToth <sandor.toth@hotmail.com>
2021-08-10 10:11:02 +0200

    stores route created in core routing module

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -5,12 +5,21 @@ import {NotFoundComponent} from "./not-found/not-found.component";
 import {ProductsContainerComponent} from "../feature/products/products-container/products-container.component";
 import {ProductsResolver} from "../shared/resolvers/products/products.resolver";
 import {AuthGuard} from "../shared/auth/auth.guard";
+import {StoresResolver} from "../shared/resolvers/stores/stores.resolver";
+import {StoresContainerComponent} from "../feature/stores/stores-container/stores-container.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/products', pathMatch: 'full'},
-  {path: 'products',
+  {
+    path: 'products',
     resolve: {products: ProductsResolver},
-    component: ProductsContainerComponent},
+    component: ProductsContainerComponent
+  },
+  {
+    path: 'stores',
+    resolve: {stores: StoresResolver},
+    component: StoresContainerComponent
+  },
   {
     path: 'login',
     canLoad: [AuthGuard],
